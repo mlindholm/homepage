ef40f4f53e03ea38a37b24e26fd2ec3231db7d64
Mathias Lindholm <mathias.lindholm@gmail.com>
2015-11-30 22:30:53 +0200

    Remove unveil plugin

--- js/main.js
+++ js/main.js
@@ -3,8 +3,4 @@ $(document).ready(function() {
   $("a[href^='http:'], a[href^='https:']").not("[href*='mathiaslindholm.com']").attr('target','_blank');
   $("a[href^='http:'], a[href^='https:']").not("[href*='localhost']").attr('target','_blank');
   $("a[href$='.pdf']").attr('target','_blank');
-
-	// lazyload images
-	$('.project-images img').unveil();
-
 });
