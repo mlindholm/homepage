d700f6648183632132dcce42b31f670ddaac54d9
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2016-12-05 00:27:10 +0200

    Add new site 👌

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  // open links and documents in new tab
   $("a[href^='http:'], a[href^='https:']").not("[href*='mathiaslindholm.com']").attr('target','_blank');
   $("a[href^='http:'], a[href^='https:']").not("[href*='localhost']").attr('target','_blank');
   $("a[href$='.pdf']").attr('target','_blank');
