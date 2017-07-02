5319434a775accf2d35f77b00085bee853553ad2
Mathias Lindholm <mathias.lindholm@gmail.com>
2017-07-02 21:56:28 +0300

    Update nighttime function

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ $( document ).ready(function() {
         e.preventDefault()
         if (nightmode == false) {
             $('.bg-white').toggleClass('bg-white bg-near-black');
-            $('.dark-gray').toggleClass('dark-gray light-gray');
-            $('.near-black').toggleClass('near-black near-white');
+            $('.near-black').toggleClass('near-black white');
+            $('.b--black-10').toggleClass('b--black-10 b--white-10');
+            $('.b--black-20').toggleClass('b--black-20 b--white-20');
             $('#inner').animate({left: '+=16px'}, 'fast')
             nightmode = true
         } else {
             $('.bg-near-black').toggleClass('bg-near-black bg-white');
-            $('.light-gray').toggleClass('light-gray dark-gray');
-            $('.near-white').toggleClass('near-white near-black');
+            $('.white').toggleClass('white near-black');
+            $('.b--white-10').toggleClass('b--white-10 b--black-10');
+            $('.b--white-20').toggleClass('b--white-20 b--black-20');
             $('#inner').animate({left: '-=16px'}, 'fast')
             nightmode = false
         }
