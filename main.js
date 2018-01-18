1e6afab4f1d69734ffa5746006195ae36248af2d
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2018-01-18 20:46:46 +0200

    Update to narrower layout, add about content and clarify link styles
    Same for both index and backpacking site. Also change inddenting to 2 spaces and update to newer tachyons version.

--- main.js
+++ main.js
@@ -7,15 +7,13 @@ $( document ).ready(function() {
         if (nightmode == false) {
             $('.bg-white').toggleClass('bg-white bg-near-black');
             $('.near-black').toggleClass('near-black white');
-            $('.b--black-10').toggleClass('b--black-10 b--white-10');
-            $('.b--black-20').toggleClass('b--black-20 b--white-20');
+            $('.dark-gray').toggleClass('dark-gray near-white');
             $('#inner').animate({left: '+=16px'}, 'fast')
             nightmode = true
         } else {
             $('.bg-near-black').toggleClass('bg-near-black bg-white');
             $('.white').toggleClass('white near-black');
-            $('.b--white-10').toggleClass('b--white-10 b--black-10');
-            $('.b--white-20').toggleClass('b--white-20 b--black-20');
+            $('.near-white').toggleClass('near-white dark-gray');
             $('#inner').animate({left: '-=16px'}, 'fast')
             nightmode = false
         }
