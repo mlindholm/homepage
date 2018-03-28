0291f2414794fcb9110fe0edf1fa3113c2957e0c
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-03-28 22:17:03 +0300

    Change font and use left align layout

--- main.js
+++ main.js
@@ -6,13 +6,11 @@ $( document ).ready(function() {
         e.preventDefault()
         if (nightmode == false) {
             $('.bg-white').toggleClass('bg-white bg-near-black');
-            $('.near-black').toggleClass('near-black white');
             $('.dark-gray').toggleClass('dark-gray near-white');
             $('#inner').animate({left: '+=16px'}, 'fast')
             nightmode = true
         } else {
             $('.bg-near-black').toggleClass('bg-near-black bg-white');
-            $('.white').toggleClass('white near-black');
             $('.near-white').toggleClass('near-white dark-gray');
             $('#inner').animate({left: '-=16px'}, 'fast')
             nightmode = false
