7b5f8c896816dbe63c79ff1c1e625d7abeff420f
Mathias Lindholm <mathias.lindholm@gmail.com>
2018-03-29 00:26:49 +0300

    Change blue tint when toggle to night mode

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ $( document ).ready(function() {
         if (nightmode == false) {
             $('.bg-white').toggleClass('bg-white bg-near-black');
             $('.dark-gray').toggleClass('dark-gray near-white');
+            $('.blue').toggleClass('blue light-blue');
             $('#inner').animate({left: '+=16px'}, 'fast')
             nightmode = true
         } else {
             $('.bg-near-black').toggleClass('bg-near-black bg-white');
             $('.near-white').toggleClass('near-white dark-gray');
+            $('.light-blue').toggleClass('light-blue blue');
             $('#inner').animate({left: '-=16px'}, 'fast')
             nightmode = false
         }
