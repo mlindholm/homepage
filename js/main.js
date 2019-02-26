64ec7969e1ca37df2a64fd76559ffe73208eab64
Mathias Lindholm <mathias.lindholm@gmail.com>
2019-02-26 23:28:41 +0200

    Cleanup nightmode js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
+var nightmode = false
+var now = new Date()
+var hours = now.getHours()
+
+function setNightMode() {
+    $('.bg-white').toggleClass('bg-white bg-oc-gray-9');
+    $('.oc-gray-8').toggleClass('oc-gray-8 oc-gray-1');
+    $('.oc-blue-8').toggleClass('oc-blue-8 oc-blue-4');
+    $('#inner').animate({left: '+=16px'}, 'fast')
+    nightmode = true
+}
+
+function setDayMode() {
+    $('.bg-oc-gray-9').toggleClass('bg-oc-gray-9 bg-white');
+    $('.oc-gray-1').toggleClass('oc-gray-1 oc-gray-8');
+    $('.oc-blue-4').toggleClass('oc-blue-4 oc-blue-8');
+    $('#inner').animate({left: '-=16px'}, 'fast')
+    nightmode = false
+}
+
+
+
 $( document ).ready(function() {
     $("a[href^='http:'], a[href^='https:']").not("[href*='mathiaslindholm.com']").attr({target: '_blank', rel: 'noopener'});
 
-    var nightmode = false
     $('#toggle').click(function(e) {
         e.preventDefault()
-        if (nightmode == false) {
-            $('.bg-white').toggleClass('bg-white bg-oc-gray-9');
-            $('.oc-gray-8').toggleClass('oc-gray-8 oc-gray-1');
-            $('.oc-blue-8').toggleClass('oc-blue-8 oc-blue-4');
-            $('#inner').animate({left: '+=16px'}, 'fast')
-            nightmode = true
+        if (!nightmode) {
+            setNightMode()
         } else {
-            $('.bg-oc-gray-9').toggleClass('bg-oc-gray-9 bg-white');
-            $('.oc-gray-1').toggleClass('oc-gray-1 oc-gray-8');
-            $('.oc-blue-4').toggleClass('oc-blue-4 oc-blue-8');
-            $('#inner').animate({left: '-=16px'}, 'fast')
-            nightmode = false
+            setDayMode()
         }
     })
 });
