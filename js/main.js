a33e52616e4a7231d9f5a0151381293b0fa2e40b
Mathias Lindholm <mathias.lindholm@gmail.com>
2019-05-01 19:49:35 +0300

    Remove footer theme toggle

--- js/main.js
+++ js/main.js
@@ -1,34 +1,9 @@
-var nightmode = false
-var now = new Date()
-var hours = now.getHours()
+var links = document.links
 
-function setNightMode() {
-    $('.bg-white').toggleClass('bg-white bg-oc-gray-9');
-    $('.oc-gray-8').toggleClass('oc-gray-8 oc-gray-1');
-    $('.oc-blue-8').toggleClass('oc-blue-8 oc-blue-4');
-    $('#inner').animate({left: '+=16px'}, 'fast')
-    nightmode = true
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+  const color = getComputedStyle(links[i], null).getPropertyValue('color').toString()
+  if (links[i].hostname != window.location.hostname && !links[i].href.match(/^mailto\:/)) {
+    links[i].target = '_blank'
+    links[i].rel = 'noopener'
+  }
 }
-
-function setDayMode() {
-    $('.bg-oc-gray-9').toggleClass('bg-oc-gray-9 bg-white');
-    $('.oc-gray-1').toggleClass('oc-gray-1 oc-gray-8');
-    $('.oc-blue-4').toggleClass('oc-blue-4 oc-blue-8');
-    $('#inner').animate({left: '-=16px'}, 'fast')
-    nightmode = false
-}
-
-
-
-$( document ).ready(function() {
-    $("a[href^='http:'], a[href^='https:']").not("[href*='mathiaslindholm.com']").attr({target: '_blank', rel: 'noopener'});
-
-    $('#toggle').click(function(e) {
-        e.preventDefault()
-        if (!nightmode) {
-            setNightMode()
-        } else {
-            setDayMode()
-        }
-    })
-});
